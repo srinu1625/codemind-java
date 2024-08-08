a2b1b86034f651121c34cf6ee7c76db3d722b58b
srinu1625 <153898551+srinu1625@users.noreply.github.com>
2024-08-08 15:47:10 +0530

    import java.util.Scanner; public class Add {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=a+b;         System.out.println(c);     } }

--- Adding_Two_Numbers.java
+++ Adding_Two_Numbers.java
@@ -3,11 +3,11 @@ public class Add
 {
     public static void main(String args[])
     {
-        int a,b;
         Scanner sc=new Scanner(System.in);
+        int a,b,c;
         a=sc.nextInt();
         b=sc.nextInt();
-        System.out.println(a+b);
-        
+        c=a+b;
+        System.out.println(c);
     }
 }
